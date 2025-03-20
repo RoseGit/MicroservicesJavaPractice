5046235bafe47f0d3d9dabdaa256c53e6f09bf01
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-20 17:25:08 -0600

    retrieve all users service

--- restful-web-services/src/main/java/com/rose/rest/webservices/restful/web/services/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/rose/rest/webservices/restful/web/services/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.rose.rest.webservices.restful.web.services.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,17 @@ public class UserDaoService {
 	
 	static {
 		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(1, "Eve", LocalDate.now().minusYears(25)));
-		users.add(new User(1, "Jim", LocalDate.now().minusYears(20)));
+		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
 	};
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id); 
+		return users.stream().filter(predicate).findFirst().get();
+	}
+	
 }
