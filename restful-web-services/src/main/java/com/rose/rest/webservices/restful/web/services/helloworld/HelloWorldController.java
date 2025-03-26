fef8dbb99337d24af388277f0bb51edceb7d3b77
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-26 13:43:42 -0600

    Internationalization

--- restful-web-services/src/main/java/com/rose/rest/webservices/restful/web/services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rose/rest/webservices/restful/web/services/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.rose.rest.webservices.restful.web.services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	/**
@@ -37,5 +46,22 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello world %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		/*
+		 * How to internationalizeed this message ?
+		 * 1. Definir en el archivo este mensaje (messages.properties)
+		 * 2. Escribir el codigo para leer el mensaje correcto  
+		 * locale lo tomames de los header de la peticion 
+		 * */
+		
+		//Obteniendo el locale desde la petición, si no se especifica toma por default el del sistema 
+		
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale );
+		//return "Hello World";
 	}
 }
