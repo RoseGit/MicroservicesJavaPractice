20180632b5bec857d7d4af17abbc0fffd374cb92
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-31 12:00:23 -0600

    Rename properties on json

--- restful-web-services/src/main/java/com/rose/rest/webservices/restful/web/services/user/User.java
+++ restful-web-services/src/main/java/com/rose/rest/webservices/restful/web/services/user/User.java
@@ -2,15 +2,19 @@ package com.rose.rest.webservices.restful.web.services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 	private Integer id;
 	@Size(min=2, message = "Name should have at leats 2 characteres")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birth Date should be in the past.")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
