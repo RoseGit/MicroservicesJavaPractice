dc4d434a84953e30b1e67d53155399727d65eb44
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-06-02 18:23:30 -0600

    Resilence4J retry default configuration

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/rose/microservices/currency/exchange/service/CircuitBreakerController.java
@@ -0,0 +1,23 @@
+package com.rose.microservices.currency.exchange.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
+	@GetMapping("/sample-api")
+	@Retry(name="default")
+	public String simpleApi() {
+		logger.info("Sample call received");
+		//provocandi un fallo y validando el uso de @Retry
+		var restTemplate = new RestTemplate().getForEntity("http://localhost:8080", String.class);
+		return restTemplate.getBody();
+	}
+}
