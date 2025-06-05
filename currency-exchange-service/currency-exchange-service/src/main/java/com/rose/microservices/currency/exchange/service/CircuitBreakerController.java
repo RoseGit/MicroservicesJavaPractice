474b9ebc4d667c00b89b78a6462c7c63c207e6af
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-06-04 22:11:53 -0600

    Configure rate limit

--- currency-exchange-service/currency-exchange-service/src/main/java/com/rose/microservices/currency/exchange/service/CircuitBreakerController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/rose/microservices/currency/exchange/service/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -18,12 +19,15 @@ public class CircuitBreakerController {
 	//El nombre debe hacer match con el archivo .properties de la configuracion, puedo tener una configuracion de intentos por servicio
 	//@Retry(name="sample-api", fallbackMethod = "hardCodedResponse")
 	//Esta anotacion permite enviar la solicitud de hardCodedResponse en automatico si detecta que el servicio falla multiples veces
-	@CircuitBreaker(name="default", fallbackMethod = "hardCodedResponse")
+	//@CircuitBreaker(name="default", fallbackMethod = "hardCodedResponse")
+	//Limita el numero de request por segundo
+	@RateLimiter(name="default")
 	public String simpleApi() {
 		logger.info("Sample call received");
 		//provocandi un fallo y validando el uso de @Retry
-		var restTemplate = new RestTemplate().getForEntity("http://localhost:8080", String.class);
-		return restTemplate.getBody();
+		/*var restTemplate = new RestTemplate().getForEntity("http://localhost:8080", String.class);
+		return restTemplate.getBody();*/
+		return "Sample rate limiter";
 	}
 	
 	//Debe aceptar un parametro de tipo Throwable
